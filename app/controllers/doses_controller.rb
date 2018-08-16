ee6da01b20854b1f0de23566f16ee60c4264f766
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2018-08-16 18:02:49 +0200

    non styled working mister cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,16 +5,19 @@ class DosesController < ApplicationController
   end
 
   def create
-      allowed_params = params.require(:doses).permit(:description, :ingredient_id, :cocktail_id)
+      allowed_params = params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
     @dose = Dose.new(allowed_params)
+     @cocktail = Cocktail.find(params[:cocktail_id])
+     @dose.cocktail_id = @cocktail.id
     if @dose.save
-      redirect_to dose_path(@cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
-
   end
 
   def destroy
+    @dose = Dose.find(params[:cocktail_id])
+    @dose.destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   resources :cocktails, only: %i[new create] do
-    resources :doses, only: %i[new create]
-    resources :ingredient, only: %i[new create]
+    resources :doses, only: %i[new create destroy]
   end
-   resources :cocktails, except: %i[new create]
+  resources :cocktails, except: %i[new create]
  end
